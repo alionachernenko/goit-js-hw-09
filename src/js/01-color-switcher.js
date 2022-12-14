8093def16953bf59547b92fcb9d37e00c4b55661
Aliona Chernenko <108655520+alionachernenko@users.noreply.github.com>
2022-12-14 13:18:06 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let changeBackgroundColorIntervalId = null;
+const isBtnDisabled = true;
+const refs = {
+  documentBody: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.stopBtn.disabled = isBtnDisabled;
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.disabled = isBtnDisabled;
+  refs.stopBtn.disabled = !isBtnDisabled;
+  changeBackgroundColorIntervalId = setInterval(changeBackgroundColor, 1000);
+}
+// refs.startBtn.disabled = !refs.stopBtn.disabled;
+// console.log(refs.startBtn);
+
+function changeBackgroundColor() {
+  refs.documentBody.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopBtnClick() {
+  clearInterval(changeBackgroundColorIntervalId);
+  refs.startBtn.disabled = !isBtnDisabled;
+  refs.stopBtn.disabled = isBtnDisabled;
+}
