41155b4009641f3e8022cb07a162e7aada7e496f
Aliona Chernenko <108655520+alionachernenko@users.noreply.github.com>
2022-12-20 18:12:39 +0200

    task 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,131 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  daysField: document.querySelector('[data-days]'),
+  hoursField: document.querySelector('[data-hours]'),
+  minutesField: document.querySelector('[data-minutes]'),
+  secondsField: document.querySelector('[data-seconds]'),
+};
+let timerIntervalId = null;
+
+refs.startBtn.disabled = true;
+
+const fp = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    onDateSelectionWindowClose(selectedDates)
+  }})
+
+
+function onDateSelectionWindowClose(selectedDates) {
+  if (selectedDates[0].getTime() < Date.now()) {
+    Notiflix.Notify.warning('Please choose a date in the future');
+    return;
+  }
+  else {
+    refs.startBtn.disabled = false
+    refs.startBtn.addEventListener('click', () => {
+      timerIntervalId = setInterval(() => {
+        if (selectedDates[0].getTime() - Date.now() < 1000) {
+          clearInterval(timerIntervalId);
+        }
+    
+        let currentTime = convertMs(selectedDates[0].getTime() - Date.now());
+        refs.daysField.textContent = addLeadingZero(currentTime.days);
+        refs.hoursField.textContent = addLeadingZero(currentTime.hours);
+        refs.minutesField.textContent = addLeadingZero(currentTime.minutes);
+        refs.secondsField.textContent = addLeadingZero(currentTime.seconds);
+      }, 1000);
+})
+}}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //   refs.startBtn.disabled = true;
+//     } else {
+//       let currentTime = convertMs(selectedDates[0].getTime() - Date.now());
+//       refs.startBtn.disabled = false;
+//       refs.daysField.textContent = currentTime.days;
+//       refs.hoursField.textContent = currentTime.hours;
+//       refs.minutesField.textContent = currentTime.minutes;
+//       refs.secondsField.textContent = currentTime.seconds;
+//     }
+//   },
+// }); // flatpickr
+
+// let selectedDate = fp.selectedDates[0];
+
+// refs.startBtn.addEventListener('click', onStartBtnClick);
+
+// function onStartBtnClick() {
+//   timerIntervalId = setInterval(() => {
+//     if (selectedDate.getTime() - Date.now() < 1000) {
+//       clearInterval(timerIntervalId);
+//     }
+
+//     let currentTime = convertMs(selectedDate.getTime() - Date.now());
+
+//     refs.daysField.textContent = currentTime.days;
+//     refs.hoursField.textContent = currentTime.hours;
+//     refs.minutesField.textContent = currentTime.minutes;
+//     refs.secondsField.textContent = currentTime.seconds;
+//   }, 1000);
+// }
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const delayInput = document.querySelector('input[name=delay]')
+const stepInput = document.querySelector('input[name=step]')
+const amountInput = document.querySelector('input[name=amount]')
+const submitButton = document.querySelector('button')
+const generatorForm = document.querySelector('form')
+
+generatorForm.addEventListener('submit', onGeneratorFormSubmit)
+
+function createPromise(position, delay){
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) =>{
+      setTimeout(() => {if (shouldResolve) {
+            resolve({
+                position: position,
+                delay: delay
+            })
+        }
+        else {
+            reject({
+                position: position,
+                delay: delay
+            })
+        }}, delay)
+    })
 }
+
+function onGeneratorFormSubmit(event){
+    event.preventDefault()
+    let counter = 1
+    let stepVal = 0
+    createPromise(counter, Number(delayInput.value))
+        .then(({position, delay}) => {Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)})
+        .catch(({position, delay}) => {Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)})
+    const intervalId = setInterval(() => {{
+        if (counter === Number(amountInput.value - 1)) clearInterval(intervalId)
+        counter += 1
+        stepVal += Number(stepInput.value)
+        createPromise(counter, stepVal + Number(delayInput.value))
+        .then(({position, delay}) => {Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)})
+        .catch(({position, delay}) => {Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)})
+    }}, stepInput.value)
+}
+
+
+
+
+
+
